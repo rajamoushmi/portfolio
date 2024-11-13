e64fa7ef14da5ee20d55b6fc5784b39fb115a4a7
DODDA RAJA MOUSHMI <116935248+rajamoushmi@users.noreply.github.com>
2024-11-13 14:23:45 +0530

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,21 @@
 'use strict';
 
-
-
-// element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
-
-// sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
-
-// sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
-
-
-// testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
-
-// modal variable
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
-
-// modal toggle function
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
-
-// add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
 
   testimonialsItem[i].addEventListener("click", function () {
@@ -48,22 +30,15 @@ for (let i = 0; i < testimonialsItem.length; i++) {
   });
 
 }
-
-// add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
-
-
-// custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
 select.addEventListener("click", function () { elementToggleFunc(this); });
-
-// add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
 
@@ -74,8 +49,6 @@ for (let i = 0; i < selectItems.length; i++) {
 
   });
 }
-
-// filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
@@ -94,7 +67,6 @@ const filterFunc = function (selectedValue) {
 
 }
 
-// add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
@@ -112,15 +84,9 @@ for (let i = 0; i < filterBtn.length; i++) {
   });
 
 }
-
-
-
-// contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
-
-// add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
 
@@ -134,15 +100,32 @@ for (let i = 0; i < formInputs.length; i++) {
   });
 }
 
+window.onload = () => {
+  const message = document.getElementById('message');
+  message.style.opacity = '1';
+};
 
+document.addEventListener("DOMContentLoaded", function () {
+  const text = "DODDA RAJA MOUSHMI";
+  const typingSpeed = 100; // Speed of typing in milliseconds
+  const target = document.querySelector(".auto-input");
+  
+  let index = 0;
 
+  function type() {
+    if (index < text.length) {
+      target.innerHTML += text.charAt(index);
+      index++;
+      setTimeout(type, typingSpeed);
+    }
+  }
 
+  type();
+});
 
-// page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
