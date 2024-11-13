a5e2f3d78e4fa62234c840397970f2b1c2111668
DODDA RAJA MOUSHMI <116935248+rajamoushmi@users.noreply.github.com>
2024-11-13 14:25:53 +0530

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
 'use strict';
 
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
+
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
+
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
+
 for (let i = 0; i < testimonialsItem.length; i++) {
 
   testimonialsItem[i].addEventListener("click", function () {
@@ -30,6 +35,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
   });
 
 }
+
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
@@ -39,6 +45,7 @@ const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
 select.addEventListener("click", function () { elementToggleFunc(this); });
+
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
 
@@ -49,6 +56,7 @@ for (let i = 0; i < selectItems.length; i++) {
 
   });
 }
+
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
@@ -84,13 +92,14 @@ for (let i = 0; i < filterBtn.length; i++) {
   });
 
 }
+
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
+
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
 
-    // check form validation
     if (form.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
@@ -123,6 +132,7 @@ document.addEventListener("DOMContentLoaded", function () {
   type();
 });
 
+
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
@@ -142,3 +152,4 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
